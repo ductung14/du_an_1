a28f40d3c71d75e4681d0ad7fc5f0adc2175c206
Duc Tung <nguyenductung224@gmail.com>
2024-11-11 09:24:46 +0700

    CRUD sanpham+danhmuc

--- du_an_1/commons/env.php
+++ du_an_1/commons/env.php
@@ -3,8 +3,8 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , value: 'http://localhost/du_an_1/du_an_1/');
-define('BASE_URL_ADMIN'       , value: 'http://localhost/du_an_1/du_an_1/admin/');
+define('BASE_URL'       , value: 'http://localhost/du_an_1/');
+define('BASE_URL_ADMIN'       , value: 'http://localhost/du_an_1/admin/');
 
 
 define('DB_HOST'    , 'localhost');
